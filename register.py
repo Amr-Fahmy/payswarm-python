89d7bfd5c27a082e9cfe96173f3bddc931bf0170
Amr Karam Fahmy <a.fahmy@cloud-11.com>
2013-05-03 20:51:21 +0200

    added user interface prototype

--- register.py
+++ register.py
@@ -7,19 +7,34 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class RegisterMain(webapp.RequestHandler):
-
-    def get(self):
+    def post(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        templateValues = {}
+        
+        # Form response is the value that goes to the HTML page to send or view the public Key
+        # TODO: Code to generate pub/prv key pair 
+        templateValues = {'publicKey': 'Public Key'}
+        
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.out.write('<form method="POST" action="registerComplete" > <input type="submit" /> </form>')
+        self.response.out.write(template.render(templateValues))
+        
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        templateValues = {}
+        
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write(template.render(templateValues))
 
-class RegisterComplete(webapp.RequestHandler):
+class RegisterRedirect(webapp.RequestHandler):
     
     def post(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('Register Completed!')
 
+
+
 application = webapp.WSGIApplication([('/', RegisterMain),
-                                      ('/registerComplete', RegisterComplete)], debug=True)
+                                      ('/registerRedirect', RegisterRedirect)], debug=True)
 
 
 def main():
