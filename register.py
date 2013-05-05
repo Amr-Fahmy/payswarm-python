3e36a131507117311a4a962de629605bdf19e751
Amr Karam Fahmy <a.fahmy@cloud-11.com>
2013-05-05 02:48:50 +0200

    Key generation and some UI

--- register.py
+++ register.py
@@ -1,40 +1,107 @@
+import datetime
+from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import jinja2
 import os
+from Crypto.PublicKey import RSA
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+class WebKeyIdentity(db.Model):
+    public_key = db.TextProperty(required=True)
+    private_key = db.TextProperty(required=True)
+    creation_date = db.DateTimeProperty(required=True, auto_now_add=True)
+    payswarm_identity = db.StringProperty()
+    payswarm_key = db.StringProperty(indexed=False)
+    payswarm_financial_account = db.StringProperty(indexed=False)
+    is_registered = db.BooleanProperty(required=True,default=False,indexed=False)
+    registration_date = db.DateTimeProperty()
+    
 class RegisterMain(webapp.RequestHandler):
-    def post(self):
+    def renderTemplate(self, webkey):
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        templateValues = {}
+        templateValues ={}
         
-        # Form response is the value that goes to the HTML page to send or view the public Key
-        # TODO: Code to generate pub/prv key pair 
-        templateValues = {'publicKey': 'Public Key'}
-        
+        if webkey:
+            templateValues = {'publicKey': webkey.public_key, 'privateKey': webkey.private_key} 
+
+        # Display results
         self.response.headers['Content-Type'] = 'text/html'
         self.response.out.write(template.render(templateValues))
+         
+    
+    def post(self):
+        # TODO: Get old key
+        webkeys = db.GqlQuery("SELECT * FROM WebKeyIdentity LIMIT 1")
+        if webkeys.count(1)>0:
+            [webkey] = webkeys
+            webkey.delete()
+        
+        # TODO: Code to generate pub/prv key pair 
+        rsakey = RSA.generate(2048)
+        publicKey = rsakey.publickey().exportKey('PEM')
+        privateKey = rsakey.exportKey('PEM')        
+        # TODO: Store the generated key pair in DB 
+        webkey = WebKeyIdentity(public_key=publicKey,
+                                private_key=privateKey)
+        webkey.put()
+        self.renderTemplate(webkey) 
+        # Display results
         
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        templateValues = {}
+        webkeys = db.GqlQuery("SELECT * FROM WebKeyIdentity LIMIT 1")
+        webkey = None
         
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.out.write(template.render(templateValues))
+        if webkeys.count(1)>0:
+            [webkey] = webkeys
+
+        self.renderTemplate(webkey)
 
 class RegisterRedirect(webapp.RequestHandler):
     
     def post(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Register Completed!')
+        configServiceIRI = self.request.get['auth_url']
+        if configServiceIRI:
+            configServiceIRI += '/.well-known/web-keys'
+            publicKey = ''
+            
+            #Send HTTPS to configServiceIRI
+            #Accept: application/ld+json; form=compacted
+            #
+            #Expected example response:
+            """
+            {
+              "@context": "https://w3id.org/payswarm/v1",
+              "publicKeyService": "https://dev.payswarm.com/i?form=register"
+            }
+            """
+            configResp = {} #parse the JSON response
+            
+            # Validate that @context == https://w3id.org/payswarm/v1, e.g.:
+            #if configResp['@context'] != 'https://w3id.org/payswarm/v1'
+
+            # Get the value of publicKeyService from response
+            regURL = configResp['publicKeyService']
+            
+            #randomNonce = Random Hex String of length 16
+            #regURL.addQueryParameter('public-key', publicKey)
+            #regURL.addQueryParameter('registration-callback', 'http://localhost:8080/registerCallback')
+            #regURL.addQueryParameter('response-nonce', randomNonce)
+            
+            #Redirect to regURL
+            self.redirect(regURL, True)
 
 
+class RegisterCallback(webapp.RequestHandler):
+    def post(self):
+        # Just print the publicKey URL
+        self.response.out.write("")      
 
 application = webapp.WSGIApplication([('/', RegisterMain),
-                                      ('/registerRedirect', RegisterRedirect)], debug=True)
+                                      ('/registerRedirect', RegisterRedirect),
+                                      ('/registerCallback', RegisterCallback )], debug=True)
 
 
 def main():
